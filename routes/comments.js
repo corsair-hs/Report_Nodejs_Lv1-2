e70d863b838ff5ba990e7196fc96d779af82771a
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-21 15:25:31 +0900

    댓글 생성, 조회부분 과제공지와 틀렸던 부분 수정

--- routes/comments.js
+++ routes/comments.js
@@ -8,15 +8,14 @@ const Comments = require('../schemas/comment.js');
 // 댓글 생성 : POST -> localhost:3000/posts/:postId/comments
 router.post('/:_postId/comments', async (req, res) => {
     try {
-        const _postId = req.params;
-        const { user, password, content } = req.body;
-        const comment = await Comments.find({ commentId })
-        if (comment.length) {
-            return res.status(404).send({ message: "이미 있는 데이터입니다." });
+        const { _postId } = req.params;
+        try {
+            const { user, password, content } = req.body; 
+            await Comments.create({ postId:_postId, user, password, content });
+            return res.status(200).json({ message: '댓글을 생성하였습니다.' })
+        } catch {
+            return res.status(400).send({ message: '댓글 내용을 입력해주세요.' });
         }
-        // return res.status(200).json({ "postId": postId, "commentId": commentId });
-        await Comments.create({ commentId, postId, user, password, content });
-        return res.status(200).json({ message: '댓글을 생성하였습니다.' })
     } catch {
         return res.status(400).send({ message: '데이터 형식이 올바르지 않습니다.' });
     }
@@ -24,15 +23,19 @@ router.post('/:_postId/comments', async (req, res) => {
 
 
 // 댓글 조회 : GET -> localhost:3000/posts/:postId/comments
-router.get('/:postId/comments', async(req, res) => {
+router.get('/:_postId/comments', async(req, res) => {
     try {
-        const postId = req.params.postId;
-        const comments = await Comments.find({"postId":postId}, {"_id":0, "__v":0, "postId":0, "password":0});
-        const [post] = await Posts.find({ postId });
-        if (!post) {
-            return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
-        }
-        res.json({ "data" : comments })
+        const { _postId } = req.params;
+        const comments = await Comments.find({"postId":_postId}, {"__v":0, "postId":0, "password":0});
+        const commentsPrint = comments.map((value)=>{
+            return {
+                commentId : value._id,
+                user : value.user,
+                content : value.content,
+                createdAt : value.createdAt
+            }
+        });
+        res.json({ "data" : commentsPrint })
     } catch (err) {
         console.log(err);
         res.status(400).send({ message: '데이터 형식이 올바르지 않습니다.' });
@@ -41,12 +44,10 @@ router.get('/:postId/comments', async(req, res) => {
 
 
 // 댓글 수정 : PUT -> localhost:3000/posts/:postId/comments/:commentId
-router.put('/:postId/comments/:commentId', async(req, res) => {
+router.put('/:_postId/comments/:_commentId', async(req, res) => {
     try {
-        // const postId = req.params.postId;
-        // const commentId = req.params.commentId;
-        const { postId, commentId } = req.params;
-        const [post] = await Posts.find({ postId });
+        const { _postId, _commentId } = req.params;
+        const [post] = await Posts.find({ _postId });
         const [comment] = await Comments.find({ commentId });
         const { password, content } = req.body;
         if (!post) {
@@ -95,3 +96,9 @@ router.delete('/:postId/comments/:commentId', async (req, res) => {
 });
 
 module.exports = router;
+
+
+// const [post] = await Posts.find({ postId });
+//     if (!post) {
+//         return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
+//     }
