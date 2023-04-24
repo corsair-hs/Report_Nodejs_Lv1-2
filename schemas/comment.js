f8d4d5974d336e29a2af3beb70c2563c13d52e87
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-25 03:27:29 +0900

    schemas/comment.js - 댓글 스키마 정의

--- schemas/comment.js
+++ schemas/comment.js
@@ -1,27 +1,38 @@
 const mongoose = require('mongoose');
 
-const commentsSchema = new mongoose.Schema({
-    postId: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    }
+const commentSchema = new mongoose.Schema({
+   postId: {
+      type: String,
+      required: true
+   },
+   userId: {
+      type: String,
+      required: true
+   },
+   nickname: {
+      type: String,
+      required: true
+   },
+   comment: {
+      type: String,
+      required: true
+   },
+   createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now
+   },
+   updatedAt: {
+      type: Date
+   }
 });
 
-module.exports = mongoose.model("comment", commentsSchema);
+commentSchema.virtual("commentId").get(function () {
+   return this._id.toHexString();
+});
+
+commentSchema.set("toJSON", {
+   virtuals: true   // JSON 형태로 가공할 때, postId를 출력시켜준다.
+});
+
+module.exports = mongoose.model("comment", commentSchema);
