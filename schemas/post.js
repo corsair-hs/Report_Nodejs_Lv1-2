2daabef848ba83881df72d7520ed4d5cfef7ab52
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-25 01:48:12 +0900

    schemas/post.js - postId virtuals 적용

--- schemas/post.js
+++ schemas/post.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-const PostsSchema = new mongoose.Schema({
-    // _id: {
-    //     type: mongoose.Types.ObjectId,
-    //     required: true
-    // },
+const PostSchema = new mongoose.Schema({
     userId: {
         type: String,
         required: true
@@ -32,14 +28,12 @@ const PostsSchema = new mongoose.Schema({
 });
 
 
-// PostsSchema.virtual("postId").get(() => {
-//     return this._id.toHexString();
-// });
+PostSchema.virtual("postId").get(function () {
+    return this._id.toHexString();
+ });
 
-// PostsSchema.set("toJSON", {
-//     virtuals: true   // JSON 형태로 가공할 때, postId를 출력시켜준다.
-// });
+PostSchema.set("toJSON", {
+    virtuals: true   // JSON 형태로 가공할 때, postId를 출력시켜준다.
+});
 
-module.exports = mongoose.model("post", PostsSchema);
-
-// default: () => mongoose.Types.ObjectId().toString()
+module.exports = mongoose.model("post", PostSchema);
--- schemas/user.js
+++ schemas/user.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  nickname: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+   nickname: {
+      type: String,
+      required: true,
+      unique: true
+   },
+   password: {
+      type: String,
+      required: true
+   }
 });
 
 
 UserSchema.virtual("userId").get(function () {
-  return this._id.toHexString();
+   return this._id.toHexString();
 });
 
 UserSchema.set("toJSON", {
-  virtuals: true   // JSON 형태로 가공할 때, userId를 출력시켜준다.
+   virtuals: true   // JSON 형태로 가공할 때, userId를 출력시켜준다.
 });
 
 
